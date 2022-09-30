9a814fd53453f0a2e4dd382ae418680134a956d8
Mariig16 <89750064+Mariig16@users.noreply.github.com>
2022-09-29 23:14:50 -0500

    Creando tarjetas con api

---
+++ prueba.js
@@ -0,0 +1,19 @@
+const tarjeta = document.getElementById("contenedorTag");
+fetch();
+
+fetch('./api.json')
+        .then((res)=>res.json())
+        .then((data)=>{
+        data.forEach((info)=>{
+          let caja = document.createElement('div');
+          caja.innerHTML = `
+          <div class="targ row m-2 card">
+          <img src="${info.foto}" class="card-img-top mt-3" alt="...">
+          <div class="card-body">
+          <h5 class="card-title">${info.precio}</h5>
+          <p class="card-text"><span>${info.title}</span></p>
+          <a href="#" class="btn btn-primary mb-1">Comprar</a>   
+          </div>`;
+          tarjeta.appendChild(caja);
+        })
+        })
