032fa69e0ab7114ec95929ebcbd5293f54ef14d5
Mariig16 <89750064+Mariig16@users.noreply.github.com>
2022-10-04 23:10:35 -0500

    Add files via upload

--- productos.js
+++ productos.js
@@ -2,6 +2,25 @@ let user = document.getElementById("user");
 let usuario = localStorage.getItem("usuario");
 user.innerHTML = usuario;
 const tabla = document.querySelector('#lista-usuarios tbody');
+let send = document.getElementById("registrarProducto");
+
+send.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const data = new FormData(send);
+  const sendData = new URLSearchParams(data);
+
+  console.log([...sendData]);
+
+  fetch('https://mariig16.github.io/apicokiesandmara/data/api.json',{
+    method: "POST",
+    body: sendData,
+  })
+  .then(respuesta => respuesta.json())
+  .then(sendData => console.log(sendData))
+  .catch(err => console.log(err))
+
+});
+
 
 function cargarUsuarios() {
   fetch('https://mariig16.github.io/apicokiesandmara/data/api.json')
