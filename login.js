9c68dfdff70bc8fc7e6bf18846146b246cc51b70
Mariig16 <89750064+Mariig16@users.noreply.github.com>
2022-10-04 22:03:02 -0500

    Actualizando

--- login.js
+++ login.js
@@ -1,46 +1,148 @@
-let formulario = document.getElementById("login");
-let formRegister = document.getElementById("register");
-let resultado =  document.getElementById("error");
-
-class Usuario {
-    constructor(user, password){
-        this.user = user;
-        this.password = password;
-    }
+class Usuarios{
+  constructor (user, pass){
+    this.user = Number(user);
+    this.pass = pass;
+  }
 }
 
-let usuarios= [];
-let usuario = new Usuario("coder1", "1234");
-let usuario2 = new Usuario("coder2", "1234");
-
-usuarios.push(usuario,usuario2);
-
-formRegister.addEventListener("submit",(e) => {
+const keyLocalStorage = "listaUsuarios";
+document.addEventListener("DOMContentLoaded", () => {
+  // fetchData();
+  let bandera = false;
+  let usuarios = []; // El arreglo global que vamos a manejar
+  
+  // Declaración de elementos del DOM
+  $newUsuarioUser = document.querySelector("#userRegister"),
+  $newUsuarioPass = document.querySelector("#passRegister"),
+  $UserRegistrado = document.querySelector("#user"),
+  $PassRegistrado = document.querySelector("#pass");
+  
+  let btnRegistro = document.getElementById("registrar")
+  let botonIngreso = document.getElementById("ingreso")
+  
+  
+  
+  btnRegistro.addEventListener("click", (e) => {
     e.preventDefault();
-    let userLogin = document.getElementById("userRegister").value;
-    let passwordLogin = document.getElementById("passRegister").value;
-
-    if (userLogin && passwordLogin) {
-        let nuevoUsuario = new Usuario (userLogin, passwordLogin);
-
-        usuarios.push(nuevoUsuario)
-        alert("Su registro ha sido exitoso", nuevoUsuario.user)
-        console.log(usuarios)
+    console.log("AQUI ESTOY")
+    const pUser = $newUsuarioUser.value;
+    const pPass = $newUsuarioPass.value;
+    validar();
+    if (bandera == true){
+      if (!pUser && pPass) {
+        return;
+      }
+      usuarios.push(new Usuarios(pUser, pPass));
+      $newUsuarioUser.value = "";
+      $newUsuarioPass.value = "";   
+      // alert("Datos de: "+pNombre +" registrados correctamente")
+      swal.fire({
+        title: "Registro exitoso",
+        text: "Usuario: "+pUser +" creado correctamente",
+        icon: "success",
+        confirmButtonText: "Ok",
+        padding: "3em",
+        background: "#f27474",
+        confirmButtonColor: "#000000",
+        allowOutsideClick: false,
+        showCloseButton: false,
+      })
+      // alertaDatosNoValidos();
+      guardarUsuariosEnLocal();
+      
+    } 
+    if(bandera == false){
+      swal.fire({
+        title: "Alerta",
+        text: "Campos incompletos",
+        icon: "warning",
+        confirmButtonText: "Ok",
+        padding: "3em",
+        background: "#ff3f28",
+        confirmButtonColor: "#000000",
+        allowOutsideClick: false,
+        showCloseButton: false,
+      })
     }
-    })
-formulario.addEventListener("submit", (e) => {
+  });
+  botonIngreso.addEventListener("click", (e) => {
     e.preventDefault();
+    console.log("Cargando...")
     let userLogin = document.getElementById("user").value;
     let passwordLogin = document.getElementById("pass").value;
-
     for (let user of usuarios) {
-        if (user.user == userLogin && user.password == passwordLogin ) {
-              alert("Acceso correcto");
-              window.location.href = "productos.html"
-              localStorage.setItem("usuario", user.user);
-        } else if ((user.user =! userLogin) && (user.password =! passwordLogin) || 
-        (user.user =! userLogin) || (user.password =! passwordLogin) ) {
-            resultado.innerHTML = `<p> Acceso incorrecto, revise su usuario o contraseña</p>`
+      if (user.user == userLogin && user.pass == passwordLogin ) {
+        swal.fire({
+          title: "Bienvenido",
+          text: "Usuario: "+userLogin +"",
+          icon: "success",
+          confirmButtonText: "Ok",
+          padding: "3em",
+          background: "#f27474",
+          confirmButtonColor: "#000000",
+          allowOutsideClick: false,
+          showCloseButton: false,
+        })
+        window.location.href = "productos.html"
+        localStorage.setItem("usuario", user.user);
+      }
+      else if  (userLogin == '' || passwordLogin == '' ){
+        if(bandera == false){
+          swal.fire({
+            title: "Alerta",
+            text: "Campos incompletos",
+            icon: "warning",
+            confirmButtonText: "Ok",
+            padding: "3em",
+            background: "#ff3f28",
+            confirmButtonColor: "#000000",
+            allowOutsideClick: false,
+            showCloseButton: false,
+          })
         }
+      }
+      else if (user.user != userLogin && user.pass != passwordLogin ){
+        if(bandera == false){
+          swal.fire({
+            title: "Error",
+            text: "Acceso incorrecto",
+            icon: "error",
+            confirmButtonText: "Ok",
+            padding: "3em",
+            background: "#ff3f28",
+            confirmButtonColor: "#000000",
+            allowOutsideClick: false,
+            showCloseButton: false,
+          })
+          
+        }
+      }
     }
-})
+  });
+  //APLICAR OPERADOR TERNARIO Y LÓGICO OR
+  function validar(){
+    const pUser = $newUsuarioUser.value;
+    const pPass = $newUsuarioPass.value;
+    const validado = (pUser == '') || (pPass == '') ? true : false;
+    validado ? bandera = false : bandera = true;
+  }
+  
+  const obtenerUsuariosRegistrado = () => {
+    const posibleLista = JSON.parse(localStorage.getItem(keyLocalStorage));
+    if (posibleLista) {
+      console.log(posibleLista);
+      return posibleLista;
+    } else {
+      return [];
+    }
+  };
+  
+  const guardarUsuariosEnLocal = () => {
+    localStorage.setItem(keyLocalStorage, JSON.stringify(usuarios));
+    
+  };    
+  usuarios = obtenerUsuariosRegistrado();     
+  
+  console.log("Hola soy la lista", ...usuarios);
+  
+});
--- productos.js
+++ productos.js
@@ -1,20 +1,24 @@
-const registro = document.getElementById("datoRegistro");
+let user = document.getElementById("user");
 let usuario = localStorage.getItem("usuario");
 user.innerHTML = usuario;
-fetch();
+const tabla = document.querySelector('#lista-usuarios tbody');
 
-fetch('https://mariig16.github.io/apicokiesandmara/data/api.json')
-        .then((res)=>res.json())
-        .then((data)=>{
-        data.forEach((info)=>{
-          let caja = document.createElement('tbody');
-          caja.innerHTML = 
-          `<tbody><tr>
-            <td>${info.id}</td>
-            <td>${info.precio}</td>
-            <td>${info.title}</td>
-            <td>${info.foto}</td>
-          </tr></tbody>`;
-          registro.appendChild(caja);
-        })
-        })
+function cargarUsuarios() {
+  fetch('https://mariig16.github.io/apicokiesandmara/data/api.json')
+  .then(respuesta => respuesta.json()) //Indicamos el formato en que se desea obtener la información
+  .then(usuarios => {
+    usuarios.forEach(usuario => {
+      const row = document.createElement('tr');
+      row.innerHTML += `
+      <td>${usuario.id}</td>
+      <td>${usuario.precio}</td>
+      <td>${usuario.title}</td>
+      <td>${usuario.foto}</td>
+      `;
+      tabla.appendChild(row);
+    });
+  }) // Aquí mostramos dicha información
+  .catch(error => console.log('Hubo un error : ' + error.message))
+}
+
+cargarUsuarios();
